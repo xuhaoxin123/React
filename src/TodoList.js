79e3bf02637604260035494d4d00b4459da37083
xuhaoxin123 <1090429158@qq.com>
2018-12-27 23:56:39 +0800

    redux练习

--- src/TodoList.js
+++ src/TodoList.js
@@ -13,25 +13,51 @@ class TodoList extends Component {
     super(props);
     console.log(store.getState())
     this.state = store.getState();
+    //更新数据
+    store.subscribe(this.handleStoreChnage);
   }
-  handleInputChnage=()=>{
-    
+  handleInputChnage=(e)=>{
+    console.log(e.target.value);
+
+    const action= {
+      type:'change_input_value',
+      value:e.target.value
+    }
+    store.dispatch(action);
+  }
+  //更新
+  handleStoreChnage=()=>{
+    this.setState(store.getState())
+  }
+  Btnlist=()=>{
+    const action = {
+      type:'add_todo_item'
+    }
+    store.dispatch(action);
+  }
+  delitem(index){
+    alert(index)
+    const action={
+      type:'delete_list_item',
+      index
+    }
+    store.dispatch(action);
   }
   render() {
     return (
       <div style={{ marginTop: "20px" }}>
         <Input
           placeholder="Basic usage"
-          value={this.state.inputValue}
+          value={this.state.inputValue}     
           style={{ width: "300px", marginRight: "30px", marginLeft: "60px" }}
           onChange={this.handleInputChnage}
         />
-        <Button type="primary">Primary</Button>
+        <Button type="primary" onClick={this.Btnlist}>Primary</Button>
         <List
         style={{marginTop:'20px',width:'600px'}}
           bordered
           dataSource={this.state.list}
-          renderItem={item => <List.Item>{item}</List.Item>}
+          renderItem={(item,index) => <List.Item onClick={this.delitem.bind(this,index)}>{item}</List.Item>}
         />
       </div>
     );
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,26 @@ const defaultState = {
   inputValue: "4324",
   list: [3,2,1]
 };
-
+//reducer 可以接受state , 绝不能修改state
 export default (state = defaultState, action) => {
+   if(action.type=== 'change_input_value'){
+     const newState = JSON.parse(JSON.stringify(state));
+     newState.inputValue= action.value;
+
+     return newState;
+   }
+   if(action.type === 'add_todo_item'){
+      const newState= JSON.parse(JSON.stringify(state));
+      newState.list.push(newState.inputValue);
+      newState.inputValue= '';
+      console.log(newState);
+      return newState;
+   }
+   if(action.type === 'delete_list_item'){
+    const newState= JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index,1);
+    console.log(newState);
+    return newState;
+ }
   return state;
 };
